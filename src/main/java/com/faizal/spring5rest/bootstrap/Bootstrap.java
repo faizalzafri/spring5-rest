281d87a1e8df977261ad2dbae0c9be0ee20874a1
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-19 19:30:08 +0530

    adding bootstrap class

---
+++ src/main/java/com/faizal/spring5rest/bootstrap/Bootstrap.java
@@ -0,0 +1,48 @@
+package com.faizal.spring5rest.bootstrap;
+
+import com.faizal.spring5rest.domain.Category;
+import com.faizal.spring5rest.repositories.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        loadCategories();
+    }
+
+    private void loadCategories() {
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        dried.setName("Dried");
+
+        Category fresh = new Category();
+        fresh.setName("Fresh");
+
+        Category exotic = new Category();
+        exotic.setName("Exotic");
+
+        Category nuts = new Category();
+        nuts.setName("Nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+
+        System.out.println("Data Loaded = " + categoryRepository.count());
+    }
+}
